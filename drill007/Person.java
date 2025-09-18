e11211d13c1c6da88674df8e1a87e299b577bf02
tsuzuki-privateDev <tsuzuki2215@gmail.com>
2025-09-18 18:29:03 +0900

    007/クラスの作成/002

---
+++ drill007/Person.java
@@ -0,0 +1,44 @@
+package drill007;
+
+public class Person {
+    private String name;
+    private int age;
+    private String gender;
+
+    public Person(String name, int age, String gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    // 情報を表示するメソッド
+    public void displayInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Gender: " + gender);
+    }
+}
