313efd722e578a5cbf0170522f99a51530fe6662
tsuzuki-privateDev <tsuzuki2215@gmail.com>
2025-09-18 20:33:40 +0900

    007クラス/クラスの宣言とオブジェクトの生成/008

--- drill007/Main.java
+++ drill007/Main.java
@@ -2,15 +2,9 @@ package drill007;
 
 public class Main {
     public static void main(String[] args) {
-        // 複数のPersonオブジェクトの生成
-        Person[] people = new Person[3];
-        people[0] = new Person("p1", 15, "male");
-        people[1] = new Person("p2", 20, "female");
-        people[2] = new Person("p3", 45, "male");
+        
+        Book bk = new Book("タイトル", "著者", 2025);
 
-        for(Person p : people) {
-            p.displayInfo();
-            System.out.println("---------------");
-        }
+        bk.displayInfo();
     }
 }
