5e11ee84e4dc499964fc4ea6f774f0d41b148337
tsuzuki-privateDev <tsuzuki2215@gmail.com>
2025-09-18 18:36:47 +0900

    007クラス/クラスの作成/003

---
+++ drill007/Book.java
@@ -0,0 +1,43 @@
+package drill007;
+
+public class Book {
+    private String title;
+    private String author;
+    private int publicationYear;
+
+    public Book(String title, String author, int publicationYear) {
+        this.title = title;
+        this.author = author;
+        this.publicationYear = publicationYear;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+
+    public void setPublicationYear(int publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+
+    public void displayInfo() {
+        System.out.println("Title: " + title);
+        System.out.println("Author:" + author);
+        System.out.println("Publication Year:" + publicationYear);
+    }
+}
