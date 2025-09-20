497e6e4e2f5db7366f4c12869b2984c93ea6dfd0
tsuzuki-privateDev <tsuzuki2215@gmail.com>
2025-09-20 12:32:10 +0900

    102 「Map修羅道」 Step02　解答

--- drill102/MapUtil.java
+++ drill102/MapUtil.java
@@ -7,10 +7,21 @@ public class MapUtil {
     public static void putMember() {
         Map<Integer, String> memberMap = new HashMap<>();
 
-        memberMap.put(101, "Yamada");
-        memberMap.put(102, "Tanaka");
-        memberMap.put(101, "Suzuki");   // key[101]がすでにあるので、値が上書きされる
+        memberMap.put(2001, "Sales");
+        memberMap.put(2002, "HR");
+        memberMap.put(2003, "Engineering");   // key[101]がすでにあるので、値が上書きされる
 
+        System.out.println("----------------");
+        System.out.println("keySet()を使って出力");
+        System.out.println("----------------");
+        for (Integer key : memberMap.keySet()) {
+            String value = memberMap.get(key);
+            System.out.println(key + ": " + value);
+        }
+
+        System.out.println("----------------");
+        System.out.println("entrySet()を使って出力");
+        System.out.println("----------------");
         for (Map.Entry<Integer, String> entry : memberMap.entrySet()) {
             Integer key = entry.getKey();
             String value = entry.getValue();
